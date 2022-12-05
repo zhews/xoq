22fd1914cf7813ffd88f245298db171678c76da7
zhews <118117386+zhews@users.noreply.github.com>
2022-12-05 16:12:34 +0100

    feat: pass statistic to game handler

--- pkg/delivery/http_server.go
+++ pkg/delivery/http_server.go
@@ -12,8 +12,10 @@ func RunHTTPServer() {
 	app := fiber.New()
 	app.Use("/game", handler.UpgradeToWebsocket)
 	qTable := domain.NewQTable()
+	statistic := &domain.Statistic{}
 	gameHandler := handler.GameHandler{
-		QTable: qTable,
+		QTable:    qTable,
+		Statistic: statistic,
 	}
 	app.Get("/game", websocket.New(gameHandler.RunGame))
 	log.Fatal(app.Listen(":8080"))
